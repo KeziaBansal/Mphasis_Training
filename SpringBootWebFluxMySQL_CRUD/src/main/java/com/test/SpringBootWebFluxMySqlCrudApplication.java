3adfd71fbe4ca50b82e4b7179f9fd05517176ca7
Kezia <167118391+KeziaBansal@users.noreply.github.com>
2026-04-03 10:56:41 +0530

    Spring Boot Web Flux MySQL CRUD

---
+++ SpringBootWebFluxMySQL_CRUD/src/main/java/com/test/SpringBootWebFluxMySqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFluxMySqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFluxMySqlCrudApplication.class, args);
+	}
+
+}
---
+++ SpringBootWebFluxMySQL_CRUD/src/main/java/com/test/repository/StoryBookRepository.java
@@ -0,0 +1,17 @@
+package com.test.repository;
+
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.test.entity.StoryBook;
+
+@Repository
+public interface StoryBookRepository extends ReactiveCrudRepository<StoryBook, Long>
+{
+	@Query("delete b from StoryBook b where b.id=?1")
+	void deleteBookById(long id);
+	
+}
+
+
