25d91325735fea5eb173ca998a194012b8308e50
Kezia <167118391+KeziaBansal@users.noreply.github.com>
2026-04-07 09:26:45 +0530

    Spring Boot REST H2 DB

---
+++ SpringBootRESTH2DB/src/main/java/com/test/SpringBootResth2DbApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResth2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResth2DbApplication.class, args);
+	}
+
+}
